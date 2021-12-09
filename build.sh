f2cc22ad07cedbb0283bfc8dccf4662788790cd3
Mykhailo Lohachov <ml.logachevsky@gmail.com>
2021-12-09 15:04:12 +0200

    setup

--- build.sh
+++ build.sh
@@ -2,10 +2,15 @@
 
 git submodule update --init --recursive --remote
 
-cd service-main
-docker build -t aoyako/service-main --no-cache . --build-arg LOCAL_REPO="off"
-docker push aoyako/service-main
-cd ..
+# cd service-main
+# docker build -t aoyako/service-main --no-cache . --build-arg LOCAL_REPO="off"
+# docker push aoyako/service-main
+# cd ..
+
+# cd service-analyzer
+# docker build -t aoyako/service-analyzer --no-cache . --build-arg LOCAL_REPO="off"
+# docker push aoyako/service-analyzer
+# cd ..
 
 cd chat-service
 docker build -t aoyako/service-chat --no-cache .
